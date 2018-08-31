bccb3cbd40e4aca9a812f251b5fd4cd9af495d3b
Jamie-Leigh Chapman <jamieleigh@users.noreply.github.com>
2018-08-31 16:25:59 +0100

    Starting Point for the App

---
+++ IMDBapp.R
@@ -0,0 +1,129 @@
+# Load in data
+load("TVratings.RData")
+
+# Define UI
+ui = fluidPage(pageWithSidebar(
+  headerPanel("IMBD User Ratings"),
+  
+  sidebarPanel(
+    selectInput(
+      inputId = "series",
+      label = "Choose a TV Series",
+      choices = unique(as.character(dataM$series.name)),
+      multiple = FALSE
+    ),
+    uiOutput("minseglen"),
+    uiOutput("pen"),
+    textOutput("cploc")
+  ),
+  mainPanel(plotOutput("data_plot"))
+))
+
+# Define server function
+server <- function(input, output) {
+  # Get data
+  data <- reactive({
+    temp <- dataM[dataM[, 1] == input$series, ]
+  })
+  
+  # Minseglen slider bounds
+  output$minseglen = renderUI({
+    sliderInput(
+      "minseglen",
+      "Minimum Segment Length",
+      min = 2,
+      max = floor(dim(data())[1] / 2),
+      value = 2,
+      step = 1
+    )
+  })
+  
+  # Get cps
+  cps <- reactive({
+    changepoint::cpt.mean(
+      data = data()$rating,
+      penalty = "CROPS",
+      pen.value = c(0.1, 1000),
+      method = "PELT",
+      minseglen = input$minseglen,
+      class = F
+    )
+  })
+  
+  #Penalty slider bounds
+  output$pen = renderUI({
+    sliderInput(
+      "pen",
+      "Penalty",
+      min = 0.1,
+      max = ceiling(tail(cps()$cpt.out[1, ], 1)),
+      value = tail(cps()$cpt.out[1, ], 1),
+      round = T
+    )
+  })
+  
+  selected_cps <- reactive({
+    cps()$changepoints[[max(which(cps()$cpt.out[1, ] <= input$pen))]]
+  })
+  
+  output$cploc <- renderText({
+    paste0("Episodes: ", paste0(selected_cps(), collapse = ","))
+  })
+  
+  output$data_plot <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    if (length(selected_cps()) > 0) {
+      ggplot2::ggplot() + ggplot2::geom_line(data = cbind(episode = 1:dim(data())[1], data()),
+                                             ggplot2::aes(x = episode, y = rating)) +
+        ggplot2::geom_rect(
+          data = cbind(episode = 1:dim(data())[1], data()),
+          ggplot2::aes(
+            ymin = 0,
+            ymax = 10,
+            xmin = episode - 0.5,
+            xmax = episode +
+              0.5
+          ),
+          fill = data()$season,
+          alpha = 0.3
+        ) +
+        ggplot2::xlab("Episode Number") + ggplot2::ylab("IMBD User Rating") + ggplot2::geom_vline(
+          xintercept =
+            selected_cps(),
+          colour = "red",
+          linetype = "dashed",
+          size = 1
+        ) +
+        ggplot2::theme(plot.margin = grid::unit(c(0, 0, 0, 0), "mm"))
+    } else{
+      ggplot2::ggplot() + ggplot2::geom_line(data = cbind(episode = 1:dim(data())[1], data()),
+                                             ggplot2::aes(x = episode, y = rating)) +
+        ggplot2::geom_rect(
+          data = cbind(episode = 1:dim(data())[1], data()),
+          ggplot2::aes(
+            ymin = 0,
+            ymax = 10,
+            xmin = episode - 0.5,
+            xmax = episode +
+              0.5
+          ),
+          fill = data()$season,
+          alpha = 0.3
+        ) +
+        ggplot2::xlab("Episode Number") + ggplot2::ylab("IMBD User Rating") +
+        ggplot2::theme(plot.margin = grid::unit(c(0, 0, 0, 0), "mm"))
+    }
+  })
+}
+
+
+# This is a function to stop the (local) server when the client is closed ( I only had this because I had students deploying the app from a USB)
+shinyServer(function(input, output, session) {
+  session$onSessionEnded(function() {
+    stopApp()
+  })
+})
+
+
+# Create shiny object
+shinyApp(ui = ui, server = server)
